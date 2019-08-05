fc276f5559556a47f95159b3fb859b5ca808b355
cai <crnkl0rd@cais-MacBook-Pro.local>
2019-08-05 09:41:52 -0400

    Not it

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,51 @@
 package hbcu.stay.ready.algor;
 
+import sun.text.IntHashtable;
+
+import java.util.HashMap;
+
 public class Solution {
 
-    public String countUniqueWords(String input){
-        return null;
+    public String countUniqueWords(String input) {
+        //loop for # unique words
+        /*int duplicateSetCount = 0;
+        HashMap<String,Integer> duplicateMap = new HashMap<String, Integer>();
+        for(int i = 0;i<input.length();i++){
+            if(!duplicateMap.containsKey(input[i])){
+                duplicateMap.put(input[i],1);
+            }else{
+                Integer keyValue = duplicateMap.get(input[i]);
+                if(keyValue == 1){
+                    duplicateSetCount++;
+                    keyValue++;
+                    duplicateMap.put(input[i],keyValue);
+                }
+            }
+
+        }
+
+
+
+        HashMap<String, Integer> wordMap = new HashMap<String, Integer>(Integer.parseInt(input));
+        String inputArray[] = input.split(" ");
+        int duplicateSetCount = 0;
+        Integer keyValue=0;
+        for (int i = 0; i < inputArray.length; i++) {
+            if (!wordMap.containsKey(inputArray[i])) {
+                wordMap.put(inputArray[i], 1);
+                ;
+            } else {
+                keyValue = wordMap.get(inputArray[i]);
+                if (keyValue == 1) {
+                    duplicateSetCount++;
+                    keyValue++;
+                    wordMap.put(inputArray[i], keyValue);
+                }
+            }
+        }
+        return String.valueOf((wordMap.keySet()));
+
+         */
+        return input;
     }
 }
